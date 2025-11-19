7cc224482dbae8c5f10a2f3d2da524ff409be6a1
Sandro Chalagashvili <sandro.chalagashvili.external@zalando.de>
2025-11-19 19:27:47 +0400

    Setup: Add TypeScript, ESLint, Prettier, and Husky

---
+++ apps/.eslintrc.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  root: true,
+  extends: [
+    'expo',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+};
